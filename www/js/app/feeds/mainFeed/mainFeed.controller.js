fb5982c250a3fe097440a157fb599df9236728ff
chenshus <chen4265@hotmail.com>
2015-02-18 21:40:58 +0200

    nothing

--- www/js/app/feeds/mainFeed/mainFeed.controller.js
+++ www/js/app/feeds/mainFeed/mainFeed.controller.js
@@ -17,13 +17,8 @@
             {name:'post', ModalService :postModal},
             {name:'checkIn', ModalService :checkInModal}
         ];
-<<<<<<< Updated upstream
         modals.forEach(function(modal){
             modal.ModalService.InitializeModal($scope);
-=======
-        popovers.forEach(function(popover){
-            popover.PopoverService.InitializePopover();
->>>>>>> Stashed changes
         });
 
 
